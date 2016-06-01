dac90524f66522cbdf3a3d2601e01705b52ebb03
Manuel G-Estañ <manuel.garcia.estan@gmail.com>
2016-06-01 11:17:58 +0200

    Initial Commit

---
+++ actions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  actions
+//
+//  Created by Manu on 1/6/16.
+//  Copyright © 2016 manuege. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
