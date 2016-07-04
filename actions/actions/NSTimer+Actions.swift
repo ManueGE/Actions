3e9844e628f320d063f35d168451c25f54cb2178
Manuel G-Estañ <manuel.garcia.estan@gmail.com>
2016-07-04 22:40:22 +0200

    update readme

--- actions/actions/NSTimer+Actions.swift
+++ actions/actions/NSTimer+Actions.swift
@@ -94,12 +94,12 @@ extension NSTimer: Actionable {
                                                         selector: action.selector,
                                                         userInfo: userInfo,
                                                         repeats: repeats)
-        action.parameter = self as! T
+        action.parameter = timer as! T
         timer.retainAction(action)
         
         return timer
     }
-    
+ 
     /**
      Creates and returns a new NSTimer object and schedules it on the current run loop in the default mode.
      - parameter timeInterval: The number of seconds between firings of the timer. If ti is less than or equal to 0.0, this method chooses the nonnegative value of 0.1 milliseconds instead.
