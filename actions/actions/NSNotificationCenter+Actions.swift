b71f5fccd52466a2656db185e8005d2abad565c6
Manuel G-Estañ <manuel.garcia.estan@gmail.com>
2016-07-10 11:55:03 +0200

    update notification and readme

--- actions/actions/NSNotificationCenter+Actions.swift
+++ actions/actions/NSNotificationCenter+Actions.swift
@@ -92,12 +92,13 @@ extension NSNotificationCenter {
     // MARK: Deprecated methods
     /**
      Adds an entry to the receiver’s dispatch table with a closure and optional criteria: notification name and sender.
-     Be sure to invoke removeObserver:name:object: when the observation is not longer needed
+     The observation lives until it is manually stopped, so be sure to invoke `stopObserving(_)` when the observation is not longer needed
+    - note: This method has been deprecated. Please se `observe(_:object:action)` instead
      - parameter to: The name of the notification for which to register the observer; that is, only notifications with this name are delivered to the observer.
      If you pass nil, the notification center doesn’t use a notification’s name to decide whether to deliver it to the observer.
      - parameter object: The object whose notifications the observer wants to receive; that is, only notifications sent by this sender are delivered to the observer. Default is `nil`.
      - parameter action: The closure which will be called when a notification with the criteria is sent
-     - deprecate: Deprecated. This method has been deprecated, please se `observe(_:object:action)` instead
+     - returns: The action that has been added to the receiver. You can catch this value to stop observing it by calling `stopObserving(_)`.
      */
     @available(*, deprecated, message="Please se `observe(_:object:action)` instead")
     public func addObserver<T: NSNotification>(to name: String?, object: AnyObject? = nil, action: (T) -> Void) -> Action {
@@ -106,11 +107,13 @@ extension NSNotificationCenter {
     
     /**
      Adds an entry to the receiver’s dispatch table with a closure and optional criteria: notification name and sender.
-     Be sure to invoke removeObserver:name:object: when the observation is not longer needed
+     The observation lives until it is manually stopped, so be sure to invoke `stopObserving(_)` when the observation is not longer needed
+     - note: This method has been deprecated. Please se `observe(_:object:action)` instead
      - parameter to: The name of the notification for which to register the observer; that is, only notifications with this name are delivered to the observer.
      If you pass nil, the notification center doesn’t use a notification’s name to decide whether to deliver it to the observer.
      - parameter object: The object whose notifications the observer wants to receive; that is, only notifications sent by this sender are delivered to the observer. Default is `nil`.
      - parameter action: The closure which will be called when a notification with the criteria is sent
+     - returns: The action that has been added to the receiver. You can catch this value to stop observing it by calling `stopObserving(_)`.
      */
     @available(*, deprecated, message="Please se `observe(_:object:action)` instead")
     public func addObserver(to name: String?, object: AnyObject? = nil, action: Void -> Void) -> Action {
